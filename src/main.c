5a8693ee54fac9adc577dddd46fed67399c35fbf
KamenA-bc <kangelovv23@gmail.com>
2026-02-22 01:28:06 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,90 +10,94 @@
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+	setbuf(stderr, NULL);
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	
-	 int server_fd, client_addr_len;
-	 struct sockaddr_in client_addr;
-	
-	 server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	 if (server_fd == -1) {
-	 	printf("Socket creation failed: %s...\n", strerror(errno));
-	 	return 1;
-	 }
-	
-	 // Since the tester restarts your program quite often, setting SO_REUSEADDR
-	 // ensures that we don't run into 'Address already in use' errors
-	 int reuse = 1;
-	 if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-	 	printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
-	
-	 struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-	 								 .sin_port = htons(4221),
-	 								 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	 if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-	 	printf("Bind failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
-	
-	 int connection_backlog = 5;
-	 if (listen(server_fd, connection_backlog) != 0) {
-	 	printf("Listen failed: %s \n", strerror(errno));
-	 	return 1;
-	 }
-	
-	 printf("Waiting for a client to connect...\n");
-	 client_addr_len = sizeof(client_addr);
-	
-	 int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	 printf("Client connected\n");
 
-	 char output[1028];
-	 recv(client_fd, output, sizeof(output), 0);
-	 if(output == -1)
-	 {
-		 printf("Error reciving request: %s...\n", strerror(errno));
-		 return 1;
-	 }
+	int server_fd, client_addr_len;
+	struct sockaddr_in client_addr;
 
-	 
-  	char *path = strstr(output, " ");
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		printf("Socket creation failed: %s...\n", strerror(errno));
+		return 1;
+	}
 
-  	if (path) 
+	// Since the tester restarts your program quite often, setting SO_REUSEADDR
+	// ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+		.sin_port = htons(4221),
+		.sin_addr = { htonl(INADDR_ANY) },
+	};
+
+	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+		printf("Bind failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
+		printf("Listen failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	printf("Waiting for a client to connect...\n");
+	client_addr_len = sizeof(client_addr);
+
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	printf("Client connected\n");
+
+	char output[1028];
+	recv(client_fd, output, sizeof(output), 0);
+	ssize_t bytes_received = recv(client_fd, output, sizeof(output) - 1, 0);
+	if (bytes_received == -1) 
+	{
+		printf("Error receiving request: %s...\n", strerror(errno));
+		return 1;
+	}
+
+
+	char *path = strstr(output, " ");
+
+	if (path) 
 	{
 
-    		path++;
+		path++;
 
-    		char *path_end = strstr(path, " ");
+		char *path_end = strstr(path, " ");
 
-    		if (path_end) 
+		if (path_end) 
 		{
 
-      			*path_end = '\0';
+			*path_end = '\0';
 
-    		}
+		}
 
-  	}
+	}
 	const char* reply;
- 	if(path && strcmp(path, "/") == 0)
+	if(path && strcmp(path, "/") == 0)
 	{
 		reply = "HTTP/1.1 200 OK\r\n\r\n";
-	} else
+	}
+	if(path && strcmp(path, "/echo/abc"){
+		reply = "HTTP/1.1 200 OK\r\n Content-Type: text/plain\r\n Contet-Length: 3\r\n\r\n abc";
+	}else 
 	{
 		reply = "HTTP/1.1 404 Not Found\r\n\r\n";
 	}
 
 	send(client_fd, reply, strlen(reply), 0);
 
-	 close(client_fd);
-	 close(server_fd);
+	close(client_fd);
+	close(server_fd);
 
 	return 0;
 }
