e8c29a7311c7fc39dacb32f9ad3093497fd3bfda
KamenA-bc <kangelovv23@gmail.com>
2026-02-25 21:41:12 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,13 +6,18 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
 
+	void *parsing_worker(void *args);
+
 int main() {
 	int server_fd = -1;
-	int client_fd = -1;
+	int *client_fd_ptr;
 	int return_status = 1; // Assume error, unless we reach the end of the code
+
+	pthread_t client_connection;
 
 	// Disable output buffering
 	setbuf(stdout, NULL);
@@ -23,57 +28,48 @@ int main() {
 	struct sockaddr_in client_addr;
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) 
-	{
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		goto cleanup;
-	}
 
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) 
-	{
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		goto cleanup;
-	}
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
 	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
 		.sin_port = htons(4221),
 		.sin_addr = { htonl(INADDR_ANY) },
 	};
 
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) 
-	{
-		printf("Bind failed: %s \n", strerror(errno));
-		goto cleanup;
-	}
+	bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 
 	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) 
-	{
-		printf("Listen failed: %s \n", strerror(errno));
-		goto cleanup;
-	}
+	listen(server_fd, connection_backlog);
+
+
 
 	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
 
-	client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if (client_fd == -1) \
+	while(1) 
 	{
-        	printf("Accept failed: %s\n", strerror(errno));
-        	goto cleanup;
-    	}
+		client_addr_len = sizeof(client_addr);
+		client_fd_ptr = malloc(sizeof(int));
+		*client_fd_ptr = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		pthread_create(&client_connection, NULL, parsing_worker, client_fd_ptr);
 
-	printf("Client connected\n");
+
+	}
+
+	close(server_fd);
+
+	return 0;
+}
+
+void *parsing_worker(void *args)
+{
+	int client_fd = *((int *) args);
+	pthread_detach(pthread_self());
+	free(args);
 
 	char output[BUFFER_SIZE];
 	ssize_t bytes_received = recv(client_fd, output, sizeof(output) - 1, 0);
-	if (bytes_received == -1) 
-	{
-		printf("Error receiving request: %s...\n", strerror(errno));
-		goto cleanup;
-	}
 
 
 	char *user_agent = strstr(output, "User-Agent");
@@ -82,7 +78,7 @@ int main() {
 		user_agent += 12;
 
 		char *user_end = strstr(user_agent, "\r\n");
-		
+
 		if(user_end)
 		{
 			*user_end = '\0';
@@ -128,24 +124,8 @@ int main() {
 
 	}
 
-	if(send(client_fd, reply, strlen(reply), 0) == -1)
-	{
-		printf("Send failed: %s\n", strerror(errno));
-        	goto cleanup;
-	}
+	send(client_fd, reply, strlen(reply), 0);
 
-	return_status = 0;
-	
-	cleanup:
-		if(client_fd != -1)
-		{
-			close(client_fd);
-		}
-		if(server_fd != -1)
-		{
-			close(server_fd);
-		}
-
-	return return_status;
+	close(client_fd);
 }
 
